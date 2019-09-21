804b391955334afe524c17977fbf805aa89996e5
Rohit kadam <rohitkadam1407@gmail.com>
2019-09-21 20:10:42 +0530

    api development is complete

---
+++ icebase/api/models/Authentication.js
@@ -0,0 +1,34 @@
+/**
+ * Authentication.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    FirstName:  { type: 'string', required: true },
+    LastName:   { type: 'string', required: true },
+    Email:      { type: 'string', required: true },
+    Password:   { type: 'string', required: true },
+    Address:    { type: 'string'},
+    Gender:     { type: 'string', required: true, isIn: ['male', 'female']}
+  },
+
+};
+
+//  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+//  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+//  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+//  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+//  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+//  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+//  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+//  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+//  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
